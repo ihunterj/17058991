100a9a057fd86a07ea9082c960db04ab4a543a40
ihunterj <jamesfurzer@outlook.com>
2020-12-07 15:01:25 +0000

    Added two new variables to GlobalC
    This will be used to keep a score and to reset the game after questionProg reaches 10, plan being to inform the user of their score as the game restarts and resets the score to 0.

--- app/src/main/java/com/example/a17058991/GlobalC.java
+++ app/src/main/java/com/example/a17058991/GlobalC.java
@@ -5,4 +5,8 @@ public class GlobalC {
     public static int rand1;
     public static int rand2;
     public static int res = 0;
+
+    // variables for SCORE, will also be used to reset the game after 5 questions
+    public static int score = 0;
+    public static int questionProg = 0;
 }
